941f892bc0b355e06b940234e2532b25cbecee7f
Gregory Parent <gregparent2006@gmail.com>
2025-09-29 16:00:12 -0400

    Finished main voting system

--- lib/voting.ml
+++ lib/voting.ml
@@ -9,6 +9,13 @@ type tally = (candidate * int) list
 (**[tally] is a type synonym of an [(candidate * int)] association list,
    connecting a candidate key with a value of their total points *)
 
+(**[lookup key] returns the value associated with [key] in an association list.
+   if key is not in the list, then it returns None*)
+let rec lookup key lst =
+  match lst with
+  | [] -> None
+  | (k, v) :: t -> if key = k then Some v else lookup key t
+
 (** [load_candidates file] is the list of candidate in [file]. Will fail if the
     file contains multiple entries per row *)
 let load_candidates file =
@@ -22,20 +29,56 @@ let load_candidates file =
 (**[load_ballots file] is the String String List loaded from [file] *)
 let load_ballots file = Csv.load file
 
-(**[compute_points candidates ballot] is an association list of each candidate’s Borda points
-   from one singular voter's ballot. *)
-let compute_points candidates ballot : (candidate * int) list=
-  (* let n = List.length candidates in *)
-  (* let points = [] in  *)
-  failwith "incomplete"
+(**[compute_points candidates ballot] is an association list of each candidate’s
+   Borda points from one singular voter's ballot. *)
+let compute_points candidates ballot : tally =
+  let n = List.length candidates in
+  let indicies = List.mapi (fun i c -> (c, i)) ballot in
+  List.map
+    (fun c ->
+      match lookup c indicies with
+      | None -> (c, 0)
+      | Some i -> (c, n - 1 - i))
+    candidates
 
-(**[combine_tallies] fully computes the final tallies by computing the sum of
-   all values in [ballots] for each candidate in [candidates] *)
-let combine_tallies candidates ballots = failwith "Incomplete"
+(**[combine_tallies combined tallies] computes the sum of the points from two
+   separate tallies by adding all tallies from [tallies] into [combined]
+   association list *)
+let combine_tallies combined tallies =
+  let add current (c, p) =
+    let current_value = lookup c current in
+    let prev =
+      match current_value with
+      | Some v -> v
+      | None -> 0
+    in
+    (*TODO : add GenAI usage for List.remove_assoc *)
+    (c, prev + p) :: List.remove_assoc c current
+  in
+  List.fold_left add combined tallies
 
 (** [tally candidates ballots] is the full Borda tally from [candidates] and
-    [ballots]. *)
-let tally candidiates ballots = failwith "Not implemented"
+    [ballots]. It starts by assigning every candidate in [candidates] as point
+    of 0. It then calls [compute_points] for each ballot in [ballots] to compute
+    them into [tally]s. [List.map computed ballots] forms a [tally list], one
+    for each ballot, and uses [combine_tallies] to fold the points into a
+    running total *)
+let tally candidates ballots =
+  let start_values = List.map (fun c -> (c, 0)) candidates in
+  let computed = compute_points candidates in
+  List.fold_left combine_tallies start_values (List.map computed ballots)
 
-(** [winners tallies] is all candidates tied for max points. *)
-let winners tallies = failwith "Not implemented"
+(** [winners tallies] is all candidates tied for max points. First we find the
+    max_points scored by using [List.fold_left] and then use [List.filter] to
+    add all elements with the winning score to an accumulator. Then we use
+    [List.map fst filter] to get the first element of each tuple, which is the
+    name of the candidate *)
+let winners tallies =
+  match tallies with
+  | [] -> []
+  | _ ->
+      let max_points =
+        List.fold_left (fun m (_, p) -> if p > m then p else m) (-1) tallies
+      in
+      let filter = List.filter (fun (_, p) -> p = max_points) tallies in
+      List.map fst filter
--- test/test_a2.ml
+++ test/test_a2.ml
@@ -1,6 +1,37 @@
 open OUnit2
 open A2.Voting
 
+(**[print_list] is a helper method I made for debugging *)
+let rec print_list = function
+  | [] -> ()
+  | (k, v) :: t ->
+      Printf.printf "Key: %s, Value: %d\n" k v;
+      print_list t
+
+let big_ice_cream_expected =
+  [
+    [ "Vanilla"; "Chocolate"; "Strawberry" ];
+    [ "Chocolate"; "Vanilla"; "Strawberry" ];
+    [ "Vanilla"; "Chocolate"; "Strawberry" ];
+    [ "Vanilla"; "Chocolate"; "Strawberry" ];
+    [ "Strawberry"; "Chocolate"; "Vanilla" ];
+    [ "Strawberry"; "Vanilla"; "Chocolate" ];
+    [ "Strawberry"; "Chocolate"; "Vanilla" ];
+    [ "Strawberry"; "Vanilla"; "Chocolate" ];
+    [ "Vanilla"; "Chocolate"; "Strawberry" ];
+    [ "Vanilla"; "Chocolate"; "Strawberry" ];
+    [ "Strawberry"; "Chocolate"; "Vanilla" ];
+    [ "Chocolate"; "Vanilla"; "Strawberry" ];
+    [ "Vanilla"; "Chocolate"; "Strawberry" ];
+    [ "Strawberry"; "Chocolate"; "Vanilla" ];
+    [ "Chocolate"; "Vanilla"; "Strawberry" ];
+    [ "Vanilla"; "Chocolate"; "Strawberry" ];
+    [ "Strawberry"; "Vanilla"; "Chocolate" ];
+    [ "Strawberry"; "Chocolate"; "Vanilla" ];
+    [ "Chocolate"; "Vanilla"; "Strawberry" ];
+    [ "Vanilla"; "Chocolate"; "Strawberry" ];
+  ]
+
 let tests =
   "test suite"
   >::: [
@@ -12,40 +43,57 @@ let tests =
            assert_bool "Checking the candidates loaded correctly from the csv"
              (expected = actual) );
          ( "Testing [load_ballots] works when given the proper file" >:: fun _ ->
-           let expected =
-             [
-               [ "Vanilla"; "Chocolate"; "Strawberry" ];
-               [ "Chocolate"; "Vanilla"; "Strawberry" ];
-               [ "Vanilla"; "Chocolate"; "Strawberry" ];
-               [ "Vanilla"; "Chocolate"; "Strawberry" ];
-               [ "Strawberry"; "Chocolate"; "Vanilla" ];
-               [ "Strawberry"; "Vanilla"; "Chocolate" ];
-               [ "Strawberry"; "Chocolate"; "Vanilla" ];
-               [ "Strawberry"; "Vanilla"; "Chocolate" ];
-               [ "Vanilla"; "Chocolate"; "Strawberry" ];
-               [ "Vanilla"; "Chocolate"; "Strawberry" ];
-               [ "Strawberry"; "Chocolate"; "Vanilla" ];
-               [ "Chocolate"; "Vanilla"; "Strawberry" ];
-               [ "Vanilla"; "Chocolate"; "Strawberry" ];
-               [ "Strawberry"; "Chocolate"; "Vanilla" ];
-               [ "Chocolate"; "Vanilla"; "Strawberry" ];
-               [ "Vanilla"; "Chocolate"; "Strawberry" ];
-               [ "Strawberry"; "Vanilla"; "Chocolate" ];
-               [ "Strawberry"; "Chocolate"; "Vanilla" ];
-               [ "Chocolate"; "Vanilla"; "Strawberry" ];
-               [ "Vanilla"; "Chocolate"; "Strawberry" ];
-             ]
-           in
            let actual = load_ballots "../data/ice_cream_ballots.csv" in
            assert_bool "Checking the candidates loaded correctly from the csv"
-             (expected = actual) );
-         ( "Testing [compute_points] works as intended"
+             (big_ice_cream_expected = actual) );
+         ( "Testing [compute_points] works as intended" >:: fun _ ->
+           let expected =
+             [ ("Chocolate", 2); ("Strawberry", 0); ("Vanilla", 1) ]
+           in
+           let ballot = [ "Chocolate"; "Vanilla"; "Strawberry" ] in
+           let tallies =
+             compute_points
+               (load_candidates "../data/ice_cream_candidates.csv")
+               ballot
+           in
+           assert_bool "Borda points were awarded as expected"
+             (tallies = expected) );
+         ( "Testing [tally] works as intended when given the ice cream test case"
          >:: fun _ ->
-          let expected = [("Vanilla",1);("Chocolate",2);("Strawberry",0)] in
-          let ballot = ["Choloate";"Vanilla";"Strawberry"] in
-          let tallies = compute_points (load_candidates "../data/ice_cream_candidates.csv") ballot in
-          assert_bool "Borda points were awarded as expected" (tallies = expected)
-          );
+           let expected =
+             [ ("Vanilla", 23); ("Strawberry", 16); ("Chocolate", 21) ]
+           in
+           let final_points =
+             tally
+               (load_candidates "../data/ice_cream_candidates.csv")
+               (load_ballots "../data/ice_cream_ballots.csv")
+           in
+           assert_bool "Borda points were awarded as expected"
+             (final_points = expected) );
+         ( "Testing [winners] works as intended when there is only 1 winner"
+         >:: fun _ ->
+           let expected_winner = [ "Vanilla" ] in
+           let final_points =
+             tally
+               (load_candidates "../data/ice_cream_candidates.csv")
+               (load_ballots "../data/ice_cream_ballots.csv")
+           in
+           let winners = winners final_points in
+           assert_bool "Winner is determined as expected"
+             (winners = expected_winner) );
+         ( "Testing [winners] works as intended when there is a tie winner"
+         >:: fun _ ->
+           let expected_winners = [ "Vanilla";"Chocolate" ] in
+           let final_points =
+             tally
+               (load_candidates "../data/ice_cream_candidates.csv")
+               (load_ballots "../data/tied_ballot.csv")
+           in
+           let winners = winners final_points in
+           assert_bool "Winner is determined as expected"
+             (winners = expected_winners) );
        ]
 
+(*TODO: make a method that compares 2 association lists without mattering about
+  order *)
 let _ = run_test_tt_main tests
